d36e6b17706cb0b8fdeb86ea3d1f494102067fc8
kimsaksith <saksithlove@gmail.com>
2025-10-20 16:02:10 +0700

    Update fillname and index.html for guest name

---
+++ Documents/Implement on Wedding Website/tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      keyframes: {
+        'fade-in': {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
+      animation: {
+        'fade-in': 'fade-in 1s ease-out forwards',
+      },
+    },
+  },
+};
